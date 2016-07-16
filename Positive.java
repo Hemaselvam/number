e1700ed911c656a8ef6f5f2579c8b83e195e3d4b
Hemaselvam <hemaabi30@gmail.com>
2016-07-16 13:24:15 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,21 @@
+public class PositiveNo {
+
+    
+    public static void main(String[] args) {
+       int s;
+       Scanner sc =new Scanner(System.in);
+       i=sc.nextInt();
+       if(s>0)
+       {
+           System.out.println("Positive no");
+       } else if(s==0)
+       {
+           System.out.println("Zero");
+       }
+       else 
+               {
+                   System.out.println("Negative no");
+               }
+    }
+
+}
