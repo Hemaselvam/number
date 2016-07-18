e5696af212cd57d69c8bd064a9f72190af4141ad
Hemaselvam <hemaabi30@gmail.com>
2016-07-18 11:35:36 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,15 @@
+public class Natural {
+
+    
+    public static void main(String[] args) {
+       int s=0;
+       int b;
+       Scanner sc=new Scanner(System.in);
+       b=sc.nextInt();
+       for(int i=0;i<=n;i++)
+       {
+           s=s+i;
+       }
+        System.out.println("sum of"+b+"numbers is:"+s);
+       }
+    }
