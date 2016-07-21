3bda5eabc7006376998b8059a3765b8b58ae8991
Hemaselvam <hemaabi30@gmail.com>
2016-07-21 15:27:22 +0530

    Add files via upload

---
+++ bst.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication34;
+
+import java.util.Scanner;
+
+
+public class JavaApplication34 {
+
+  public static void main(String ar[])throws Exception
+{
+    int n,i,j,k;
+    Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+    k=sc.nextInt();
+int a[]=new int[100];
+for(i=0;i<n;i++)
+    a[i]=sc.nextInt();
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(t==a[i]+a[j])
+    System.out.println(" sum of two elements to give a target elemets is:"+a[i]+"\t"+a[j]);
+}
+}
+
+}
+}
