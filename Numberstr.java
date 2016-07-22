5f7337d4bbad2609a7dbbacf6ae49424dd7f094d
Hemaselvam <hemaabi30@gmail.com>
2016-07-22 15:51:20 +0530

    Add files via upload

---
+++ Numberstr.java
@@ -0,0 +1,31 @@
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the values");
+int i=s.nextInt();
+switch(i)
+{
+case 1:
+System.out.println("b");
+break;
+case 2:
+System.out.println("c");
+break;
+case 3:
+System.out.println("d");
+break;
+case 4:
+System.out.println("e");
+break;
+case 5:
+System.out.println("f");
+break;
+case 12258:
+System.out.println("bcdef");
+break;
+default:
+System.out.println("invalid");
+break;
+}
+}
+}
