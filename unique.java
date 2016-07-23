2f86d606065ad3c5b521c38a2e0a78ffe2b57858
Hemaselvam <hemaabi30@gmail.com>
2016-07-23 12:52:58 +0530

    Add files via upload

--- unique.java
+++ unique.java
@@ -1,33 +1,20 @@
-public static void main(String[] args) 
-    {
- 
-    Scanner sc=new Scanner(System.in);
-    int b=sc.nextInt();
-    int[] a=new int[b];
-    for(int i=0;i<b;i++)
-        {
-            a[i]=sc.nextInt();
-            
-        }
-        for(int i=0;i<b-1;i++)
-     {
-         for(int j=i+1;j<b;j++)
-         {
-             if(a[i]==a[j])
-             {
-                 a[j]=' ';
-
-             a[i]=' ';
-             }
-         }
-         
-     }
-  
-
-    for(int i=0;i<b;i++)
-        { 
-            if(a[i]!=' ')
-            System.out.print(""+a[i]);
-        }
-    }
+public static void main(String ar[])throws Exception
+{
+int n,p;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+int ar1[]=new int[n];
+for(int i=0;i<n;i++)
+ar1[i]=sc.nextInt();
+for(int i=0;i<n;i++)
+{
+p=ar1[i];
+for(int j=i+1;j<n;j++)
+{
+if(p==ar1[j])
+System.out.println("first repeated integer is:"+p);
+break;
+}
+}
+}
 }
